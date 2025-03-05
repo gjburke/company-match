7c1c6595f170a5429f2108f0e21437567e301b47
gjburke <148822250+gjburke@users.noreply.github.com>
2025-03-04 19:05:18 -0600

    Added React Practice, some instructions, and my own practice app (#11)
    * Creating React Practice and Instructions
    
    * Added my own temp project, finished up instructions
    
    * Added by Understanding component
    
    * Added by Understanding component
    
    ---------
    
    Co-authored-by: gjburke2 <gjburke2@illinois.edu>

---
+++ React Practice/Griffin/my-app/src/Understanding.tsx
@@ -0,0 +1,12 @@
+export default function Understanding() {
+    return (
+        <div>
+            I have a pretty solid grasp on the React Basics, up to around hooks.
+
+            But in general, I haven't worked with too many advanced systems or large React apps.
+
+            I heard they get pretty complicated when you have a huge site with a lot of functionality,
+            but that's why we're keeping it simple here.
+        </div>
+    );
+}
