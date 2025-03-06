f2c2f3b05f5f59a83d0232efd730f823d189e180
saranb23 <burapachaisrisaran@gmail.com>
2025-03-06 12:48:04 -0600

    Create a react practice app, adding a component called Understanding

---
+++ React Practice/Mighty/app-testing/src/Understanding.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Understanding() { 
+    return (<div>
+        React is a component-based language. So a lot of the things we see in the website will actually be compartmentalized as reusable containers.
+    </div>
+    ); 
+}
+
+export default Understanding; 
